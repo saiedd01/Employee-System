f098a041ccf752613cd7816b86bdf51732ac32a0
saiedd01 <sa3edahmed20@gmail.com>
2022-12-14 17:42:07 +0200

    Connection with Database

---
+++ Employee System/Functions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_System
+{
+    class Functions
+    {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string connstr;
+
+        public Functions()
+        {
+            connstr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\Documents\EmpDB.mdf;Integrated Security=True;Connect Timeout=30";
+            Con = new SqlConnection(connstr);
+            Cmd = new SqlCommand();
+            Cmd.Connection = Con;
+        }
+        public DataTable GetData(string Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query, connstr);
+            sda.Fill(dt);
+            return dt;
+        }
+        public int SetData(string Query)
+        {
+            int cnt = 0;
+            if (Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
+            }
+            Cmd.CommandText = Query;
+            cnt = Cmd.ExecuteNonQuery();
+            return cnt;
+        }
+    }
+}
