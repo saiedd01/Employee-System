d5121c6167a47c52f89810db9432a12143a49306
saiedd01 <sa3edahmed20@gmail.com>
2022-12-15 21:40:24 +0200

    Add Function Show Salary

--- Employee System/Salaries.cs
+++ Employee System/Salaries.cs
@@ -12,9 +12,24 @@ namespace Employee_System
 {
     public partial class Salaries : Form
     {
+        Functions Con;
         public Salaries()
         {
             InitializeComponent();
+            Con = new Functions();
+            ShowSal();
+            
+        }
+
+        private void ShowSal()
+        {
+            string Query = "Select * from Salary";
+            Salarylist.DataSource = Con.GetData(Query);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
